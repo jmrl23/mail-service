e14b8375e195a15c227d26c32e7916186bbc4a7e
Jomariel Gaitera <jojogaitera@gmail.com>
2023-12-13 00:05:50 +0800

    Optimize

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -6,43 +6,45 @@ import { MailSendDto } from '../dtos/MailSend.dto';
 
 export const controller = Router();
 
-controller
+(async function () {
+  controller
 
-  .get(
-    '/',
-    wrapper(function (_request, response) {
-      response.status(200).end('OK');
-    }),
-  )
+    .get(
+      '/',
+      wrapper(function (_request, response) {
+        response.status(200).end('OK');
+      }),
+    )
 
-  /**
-   * @openapi
-   *
-   * /send:
-   *  post:
-   *    summary: send mail
-   *    requestBody:
-   *      content:
-   *        application/json:
-   *          schema:
-   *            $ref: '#/components/schemas/MailSend'
-   *    responses:
-   *      '200':
-   *        description: Successful response
-   *        content:
-   *          application/json: {}
-   */
+    /**
+     * @openapi
+     *
+     * /send:
+     *  post:
+     *    summary: send mail
+     *    requestBody:
+     *      content:
+     *        application/json:
+     *          schema:
+     *            $ref: '#/components/schemas/MailSend'
+     *    responses:
+     *      '200':
+     *        description: Successful response
+     *        content:
+     *          application/json: {}
+     */
 
-  .post(
-    '/send',
-    authorizationMiddleware,
-    validate('BODY', MailSendDto),
-    wrapper(async function (request) {
-      const mailService = await MailService.getInstance();
-      const mail = await mailService.sendMail(request.body);
+    .post(
+      '/send',
+      authorizationMiddleware,
+      validate('BODY', MailSendDto),
+      wrapper(async function (request) {
+        const mailService = await MailService.getInstance();
+        const mail = await mailService.sendMail(request.body);
 
-      return {
-        mail,
-      };
-    }),
-  );
+        return {
+          mail,
+        };
+      }),
+    );
+})();
--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,26 +1,36 @@
 import {
   type Transporter,
   type SentMessageInfo,
+  type TransportOptions,
   createTransport,
-  TransportOptions,
 } from 'nodemailer';
 import { CacheService } from './cache.service';
-import { google } from 'googleapis';
+import { google, type Auth } from 'googleapis';
 import { caching } from 'cache-manager';
 import { MailSendDto } from '../dtos/MailSend.dto';
 import env from 'env-var';
 
 export class MailService {
   private static instance: MailService;
+  private transporter: Transporter<SentMessageInfo>;
 
   private constructor(
-    private readonly transporter: Transporter<SentMessageInfo>,
+    private readonly oauth2Client: Auth.OAuth2Client,
     private readonly cacheService: CacheService,
   ) {}
 
-  public static async createInstance(options?: unknown): Promise<MailService> {
+  public static async createInstance(): Promise<MailService> {
+    const oauth2Client = new google.auth.OAuth2(
+      env.get('GOOGLE_CLIENT_ID').asString(),
+      env.get('GOOGLE_CLIENT_SECRET').asString(),
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: env.get('GOOGLE_REFRESH_TOKEN').asString(),
+    });
+
     const instance = new MailService(
-      createTransport(options as TransportOptions),
+      oauth2Client,
       await CacheService.createInstance(
         caching('memory', {
           ttl: 60 * 1000 * 60 * 6,
@@ -32,52 +42,43 @@ export class MailService {
   }
 
   public static async getInstance(): Promise<MailService> {
-    const accessTokenCacheKey = 'MailService:var(accessToken)';
-
     if (!MailService.instance) {
-      const oauth2Client = new google.auth.OAuth2(
-        env.get('GOOGLE_CLIENT_ID').asString(),
-        env.get('GOOGLE_CLIENT_SECRET').asString(),
-      );
-
-      oauth2Client.setCredentials({
-        refresh_token: env.get('GOOGLE_REFRESH_TOKEN').asString(),
-      });
-
-      const accessToken = await oauth2Client.getAccessToken();
-
-      MailService.instance = await MailService.createInstance({
-        host: env.get('SMTP_TRANSPORT_HOST').asString(),
-        port: env.get('SMTP_TRANSPORT_PORT').default(465).asPortNumber(),
-        secure: env.get('SMTP_TRANSPORT_SECURED').default('false').asBool(),
-        auth: {
-          type: 'OAuth2',
-          user: env.get('SMTP_TRANSPORT_USER').asString(),
-          clientId: env.get('GOOGLE_CLIENT_ID').asString(),
-          clientSecret: env.get('GOOGLE_CLIENT_SECRET').asString(),
-          refreshToken: env.get('GOOGLE_REFRESH_TOKEN').asString(),
-          accessToken,
-        },
-      });
-
-      MailService.instance.cacheService.set(accessTokenCacheKey, accessToken);
-    }
-
-    const accessTokenCache =
-      await MailService.instance.cacheService.get<string>(accessTokenCacheKey);
-
-    if (!accessTokenCache) {
-      MailService.instance = null as unknown as MailService;
-
-      return await MailService.getInstance();
+      MailService.instance = await MailService.createInstance();
     }
 
     return MailService.instance;
   }
 
   public async sendMail(mailSendDto: MailSendDto): Promise<unknown> {
+    await this.refreshTransport();
+
     const response = await this.transporter.sendMail(mailSendDto);
 
     return response;
+  }
+
+  private async refreshTransport(): Promise<void> {
+    const cacheKey = 'MailService:refreshTransport():state(isTransportActive)';
+    const isTransporterActive = await this.cacheService.get<boolean>(cacheKey);
+
+    if (isTransporterActive) return;
+
+    const accessToken = await this.oauth2Client.getAccessToken();
+
+    this.transporter = createTransport({
+      host: env.get('SMTP_TRANSPORT_HOST').asString(),
+      port: env.get('SMTP_TRANSPORT_PORT').default(465).asPortNumber(),
+      secure: env.get('SMTP_TRANSPORT_SECURED').default('false').asBool(),
+      auth: {
+        type: 'OAuth2',
+        user: env.get('SMTP_TRANSPORT_USER').asString(),
+        clientId: env.get('GOOGLE_CLIENT_ID').asString(),
+        clientSecret: env.get('GOOGLE_CLIENT_SECRET').asString(),
+        refreshToken: env.get('GOOGLE_REFRESH_TOKEN').asString(),
+        accessToken,
+      },
+    } as TransportOptions);
+
+    await this.cacheService.set(cacheKey, true, 60 * 1000 * 60 * 6);
   }
 }
